d960a8af8a0e37a3356bec80c713792c47b4d96e
Abdullah <abdullaharif893@gmail.com>
2025-02-16 03:21:41 +0500

    new

--- src/sanity/schemaTypes/product.ts
+++ src/sanity/schemaTypes/product.ts
@@ -1,60 +1,59 @@
-// schemas/product.ts
-const product = {
-	name: 'product',
-	title: 'Product',
-	type: 'document',
-	fields: [
-	  {
-		name: 'name',
-		title: 'Name',
-		type: 'string',
-		validation: (Rule: any) => Rule.required(),
-	  },
-	  {
-		name: 'slug',
-		title: 'Slug',
-		type: 'slug',
-		options: {
-		  source: 'name',
-		  maxLength: 96,
-		},
-		validation: (Rule: any) => Rule.required(),
-	  },
-	  {
-		name: 'price',
-		title: 'Price',
-		type: 'number',
-		validation: (Rule: any) => Rule.required().positive(),
-	  },
-	  {
-		name: 'productType',
-		title: 'Product Type',
-		type: 'string',
-		options: {
-		  list: [
-			{ title: 'Eau de Parfum', value: 'eau-de-parfum' },
-			{ title: 'Eau de Toilette', value: 'eau-de-toilette' },
-			{ title: 'Perfume Oil', value: 'perfume-oil' },
-		  ],
-		},
-		validation: (Rule: any) => Rule.required(),
-	  },
-	  {
-		name: 'image',
-		title: 'Image',
-		type: 'image',
-		options: {
-		  hotspot: true,
-		},
-		validation: (Rule: any) => Rule.required(),
-	  },
-	  {
-		name: 'description',
-		title: 'Description',
-		type: 'text',
-		validation: (Rule: any) => Rule.max(500),
-	  },
-	],
-  };
-  
-  export default product;
+import { defineType, defineField } from 'sanity';
+
+export default defineType({
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      validation: (Rule) => Rule.required().positive(),
+    }),
+    defineField({
+      name: 'productType',
+      title: 'Product Type',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'Eau de Parfum', value: 'eau-de-parfum' },
+          { title: 'Eau de Toilette', value: 'eau-de-toilette' },
+          { title: 'Perfume Oil', value: 'perfume-oil' },
+        ],
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      validation: (Rule) => Rule.max(500),
+    }),
+  ],
+});
