8ccf6b84bdae06da7a8eeb332987b119b24ec4ab
AbdullahArif17 <abdullaharif893@gmail.com>
2026-09-19 03:21:25 +0500

    Update WhatsApp number to 03201267017 for orders and contact

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,10 @@
 export const STORE_NAME = "Timeless Collections";
 export const STORE_DESCRIPTION = "Curated luxury collections defining sophistication and style.";
-export const WHATSAPP_NUMBER = "+923073532413";
-export const WHATSAPP_DISPLAY = "+92 307 3532413";
+export const WHATSAPP_NUMBER = "+923201267017";
+export const WHATSAPP_DISPLAY = "0320 1267017";
 
 export const SOCIAL_LINKS = {
   instagram: "https://www.instagram.com/timeless_fragrance_official/",
   facebook: "https://www.facebook.com/profile.php?id=61572591627602",
-  whatsapp: `https://wa.me/${WHATSAPP_NUMBER.replace(/[^0-9]/g, "")}`,
+  whatsapp: `https://wa.me/923201267017`,
 };
